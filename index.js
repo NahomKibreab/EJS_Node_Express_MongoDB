39b680fbb3e1aa8b7a1a1cdf6c86eb5a97b05dfe
NahomKibreab <nknahom@gmail.com>
2021-02-20 20:43:03 -0700

    New Form Query added

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const path = require('path');
 const port = 3000;
 const Product = require('./models/product');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 mongoose.connect('mongodb://localhost/shopApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -18,13 +21,32 @@ db.once('open', function () {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/product/:id', async (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/product/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/product/new', async (req, res) => {
+  const newProduct = Product(req.body);
+  await newProduct.save();
+  res.redirect('/');
+});
+
+app.get('/product/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('show', { product });
 });
 
-app.use('/', async (req, res) => {
+app.get('/product/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  const categories = ['vegetable', 'fruit', 'dairy'];
+  res.render('Edit', { product, categories });
+});
+
+app.get('/', async (req, res) => {
   const products = await Product.find({});
   res.render('home', { products });
 });
